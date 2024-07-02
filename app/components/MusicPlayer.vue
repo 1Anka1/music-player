2ca0fb07203e8c425eed6e60dcc15ba7c7a28d04
1Anka1 <annakasjanenko02@gmail.com>
2024-07-02 22:04:44 +0200

    Сode refactoring

--- app/components/MusicPlayer.vue
+++ app/components/MusicPlayer.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/swiper-bundle.css'
+import { Mousewheel } from 'swiper/modules'
+
 const isOpened = ref(false)
 
 function toggle() {
   isOpened.value = !isOpened.value
 }
+
+const slides = ref([
+  { image: 'https://via.placeholder.com/600x300?text=Slide+1' },
+  { image: 'https://via.placeholder.com/600x300?text=Slide+2' },
+  { image: 'https://via.placeholder.com/600x300?text=Slide+3' },
+  { image: 'https://via.placeholder.com/600x300?text=Slide+4' },
+  { image: 'https://via.placeholder.com/600x300?text=Slide+5' },
+  { image: 'https://via.placeholder.com/600x300?text=Slide+6' },
+])
 </script>
 
 <template>
@@ -24,4 +37,16 @@ function toggle() {
       кнопки
     </div>
   </div>
+  <Swiper
+    :slides-per-view="3"
+    :space-between="5"
+    :pagination="{ clickable: true }"
+    :mousewheel="true"
+    :keyboard="true"
+    :modules="[Mousewheel]"
+  >
+    <SwiperSlide v-for="(slide, index) in slides" :key="index">
+      <img :src="slide.image">
+    </SwiperSlide>
+  </Swiper>
 </template>
---
+++ app/playlist/index.ts
@@ -0,0 +1,6 @@
+export * from './composables/usePlaylist'
+
+export * from './models/Playlist'
+
+export { default as PlaylistCard } from './ui/PlaylistCard.vue'
+export { default as PlaylistSwiper } from './ui/PlaylistSwiper.vue'
